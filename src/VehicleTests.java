9dfa4ffd04a48917b06d5797c6cf7ad06c95bfcb
Maggie Li <maggiel36@nycstudents.net>
2022-03-23 22:16:24 -0400

    added test code

--- src/VehicleTests.java
+++ src/VehicleTests.java
@@ -114,5 +114,13 @@ public class VehicleTests
     Vehicle myVehicle3 = new Vehicle("BB8123", 7.0, 3);
     myVehicle3.printInfo();
 
+    Car myCar3 = new Car("MK4567", 6.75, 4, true);
+    myCar3.printInfo();
+
+    Truck myTruck3 = new Truck("TB12MX", 15.50, 2, 4, true);
+    myTruck3.printInfo();
+
+    Taxi myTaxi4 = new Taxi("TX1412", 13.75, 4, true, 2.0);
+    myTaxi4.printInfo();
   }
 }
